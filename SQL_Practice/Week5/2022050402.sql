e2fae0fd5d506b4a62d504b0c1ddac863f5f5fdc
gyeomii <kimsg7388@gmail.com>
2022-05-04 17:23:50 +0900

    Update 2022050402.sql

--- SQL_Practice/Week5/2022050402.sql
+++ SQL_Practice/Week5/2022050402.sql
@@ -85,8 +85,68 @@
  
  3)FOR 문
   . 수행횟수가 중요하거나 횟수를 아는 경우 사용
-  (일반적 FOR문 사용형식)
-    FOR 인덱스 IN [REVERSE] 초기값 ~ 최종값 LOOP
+  . 인덱스는 시스템에서 자동 생성
+  . REVERSE 는 출력값을 역순으로 출력함
+  . 초기값..최종값까지 1씩 증가('..'을 반드시 기술)
+  . 최종값까지 도달하면 알아서 반복문을 벗어남
+  
+ (일반적 FOR문 사용형식)
+    FOR 인덱스 IN [REVERSE] 초기값..최종값 LOOP
       반복처리명령문(들);
     END LOOP;
-  (
+ (사용예) 구구단 6단 출력
+    DECLARE
+    BEGIN
+     FOR I IN 1..9 LOOP
+       DBMS_OUTPUT.PUT_LINE('6 * '||I||' = '||I * 6);
+     END LOOP;
+    END;
+    
+ (REVERSE사용)
+    DECLARE
+    BEGIN
+     FOR I IN REVERSE 1..9 LOOP
+       DBMS_OUTPUT.PUT_LINE('6 * '||I||' = '||I * 6);
+     END LOOP;
+    END;
+    
+ (커서용 FOR문 사용형식)
+    FOR 레코드명 IN 커서명|커서용SELECT문 LOOP
+      커서처리문;
+    END LOOP;
+   . '커서명|커서용SELECT문' : 커서를 선언부에서 선언한 경우 커서명을 기술,
+      in-line 형식으로 커서의 SELECT 문을 직접 기술 가능
+   . OPEN, FETCH, CLOSE문을 사용하지 않음
+   . 커서 내의 컬럼의 참조는 '레코드명.커서컬럼명' 형식으로 참조함
+   
+ (사용예)사원테이블에서 직무코드가 'SA_MAN'인 사원 정보를 익명블록을 사용하여 출력하시오.
+        출력할 내용은 사원번호, 사원명, 입사일이다.
+        
+   (선언부에 커서를 선언)
+    DECLARE
+      CURSOR CUR_EMP03 IS
+        SELECT EMPLOYEE_ID, EMP_NAME, HIRE_DATE
+          FROM HR.EMPLOYEES
+         WHERE JOB_ID = 'SA_MAN';
+    BEGIN
+      FOR REC IN CUR_EMP03 LOOP
+        DBMS_OUTPUT.PUT_LINE('사원번호 : '||REC.EMPLOYEE_ID);
+        DBMS_OUTPUT.PUT_LINE('사원명 : '||REC.EMP_NAME);
+        DBMS_OUTPUT.PUT_LINE('입사일 : '||REC.HIRE_DATE);
+        DBMS_OUTPUT.PUT_LINE('-------------------------');
+      END LOOP;
+    END;
+    
+  (FOR문에 커서용 SELECT문을 선언)   
+    DECLARE
+    BEGIN
+      FOR REC IN (SELECT EMPLOYEE_ID, EMP_NAME, HIRE_DATE
+                    FROM HR.EMPLOYEES
+                   WHERE JOB_ID = 'SA_MAN')
+      LOOP
+        DBMS_OUTPUT.PUT_LINE('사원번호 : '||REC.EMPLOYEE_ID);
+        DBMS_OUTPUT.PUT_LINE('사원명 : '||REC.EMP_NAME);
+        DBMS_OUTPUT.PUT_LINE('입사일 : '||REC.HIRE_DATE);
+        DBMS_OUTPUT.PUT_LINE('-------------------------');
+      END LOOP;
+    END;    
