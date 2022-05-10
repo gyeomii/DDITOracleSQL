fb0c83abec216ca591777a032ede5a4b7567bdba
gyeomii <kimsg7388@gmail.com>
2022-05-10 17:12:26 +0900

    Update 2022051001.sql

--- SQL_Practice/Week6/2022051001.sql
+++ SQL_Practice/Week6/2022051001.sql
@@ -41,3 +41,23 @@ SELECT PROD_ID, PROD_NAME,
        FN_PRODAVGQTY(2020, PROD_ID) AS AVG_QTY_2020,
        FN_PRODAVGQTY(2021, PROD_ID) AS AVG_QTY_2021
   FROM PROD;
+ (사용예)상품코드, 상품명, 대분류코드, 대분류명을 출력하자
+        함수를 생성하여 대분류명을 처리해보자, 함수명: FN_PRODNM
+/        
+    CREATE OR REPLACE FUNCTION FN_PRODNM
+    (P_PROD_LGU IN VARCHAR2)
+    RETURN VARCHAR2
+    IS
+        V_NM VARCHAR2(30);
+    BEGIN
+        SELECT LPROD_NM INTO V_NM
+          FROM LPROD
+         WHERE LPROD_GU = P_PROD_LGU;
+        
+        RETURN V_NM;
+    END;
+/
+    SELECT PROD_ID, PROD_NAME, PROD_LGU,
+           FN_PRODNM(PROD_LGU) AS PROD_NM
+      FROM PROD;
+/
